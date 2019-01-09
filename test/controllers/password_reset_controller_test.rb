a7e3f0420e5ca81bb329aa87bd6ac7d00be7c87d
yanfuhai <yan_fuhai@foxmail.com>
2019-01-09 16:55:12 +0800

    Invoke tests.

--- test/controllers/password_reset_controller_test.rb
+++ test/controllers/password_reset_controller_test.rb
@@ -1,19 +1,7 @@
 require 'test_helper'
 
 class PasswordResetControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get password_reset_create_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get password_reset_destroy_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get password_reset_update_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
--- test/controllers/teach_relationships_controller_test.rb
+++ test/controllers/teach_relationships_controller_test.rb
@@ -1,19 +1,7 @@
 require 'test_helper'
 
 class TeachRelationshipsControllerTest < ActionDispatch::IntegrationTest
-  test "should get my_courses" do
-    get teach_relationships_my_courses_url
-    assert_response :success
-  end
-
-  test "should get submit_grades" do
-    get teach_relationships_submit_grades_url
-    assert_response :success
-  end
-
-  test "should get notification" do
-    get teach_relationships_notification_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
